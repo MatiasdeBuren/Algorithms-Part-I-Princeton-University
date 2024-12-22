386d3bcbd0f040913f738dd318482150f7d48ce9
MatiasdeBuren <64704995+MatiasdeBuren@users.noreply.github.com>
2024-12-21 22:42:09 -0300

    Refactor: Update Percolation and PercolationStats classes for improved functionality and clarity. Added second UF to prevent backwash when using isFull method.

--- Module 2/Percolation.java
+++ Module 2/Percolation.java
@@ -1,29 +1,32 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
-    public boolean openSites[][];
-    private static int size;
+    private boolean openSites[][];
+    private int size;
     private int openSitesCount = 0;
-    public static WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF backWachPrevention; //Wont have the bottom virtual site, used as a second union find to check if a site is full
      // test client (optional)
      public static void main(String[] args){
-        size = 5;
-        Percolation p = new Percolation(size);
-        //Ill add 2 to the size to account for the virtual top and bottom sites, which will be at n^2 and n^2 + 1
-        uf = new WeightedQuickUnionUF( (size * size) + 2);
-        p.open(1, 1);
-        p.open(2,1);
-        p.open(3,1);
-        p.open(4,1);
-        p.open(4,2);
-        p.open(5,2);
-        StdOut.println("Connected (1, 1) to top virtual site: " + (uf.find(0)==uf.find(25)));
-        StdOut.println("Connected (1, 1) to bottom virtual site: " + (uf.find(0)==uf.find(26)));
-        StdOut.println("Percolates: " + p.percolates());
+        // size = 5;
+        // Percolation p = new Percolation(size);
+        // //Ill add 2 to the size to account for the virtual top and bottom sites, which will be at n^2 and n^2 + 1
+        // uf = new WeightedQuickUnionUF( (size * size) + 2);
+        // p.open(1, 1);
+        // p.open(2,1);
+        // p.open(3,1);
+        // p.open(4,1);
+        // p.open(4,2);
+        // p.open(5,2);
+        // StdOut.println("Connected (1, 1) to top virtual site: " + (uf.find(0)==uf.find(25)));
+        // StdOut.println("Connected (1, 1) to bottom virtual site: " + (uf.find(0)==uf.find(26)));
+        // StdOut.println("Percolates: " + p.percolates());
     }
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n){
+        size = n;
+        uf = new WeightedQuickUnionUF( (n * n) + 2);
+        backWachPrevention = new WeightedQuickUnionUF( (n * n) + 1);
         if (n <= 0) {
             throw new IllegalArgumentException("n must be greater than 0");
         }
@@ -39,17 +42,17 @@ public class Percolation {
             openSites[row - 1][col - 1] = true;
             openSitesCount++;
             int sitesToConnect[][] = {{row + 1, col}, {row - 1, col}, {row, col + 1}, {row, col - 1}};
-            int siteId = (row - 1) * size + (col - 1);
             for (int i = 0; i < 4; i++){
                 int r = sitesToConnect[i][0];
                 int c = sitesToConnect[i][1];
                 if (r >= 1 && r <= size && c >= 1 && c <= size && isOpen(r, c)){
-                    uf.union( siteId, (r - 1) * size + (c - 1) );
-                    //StdOut.println("Connected (" + row + ", " + col + ") to (" + r + ", " + c + ")");
+                    uf.union( (row - 1) * size + (col - 1), (r - 1) * size + (c - 1) );
+                    backWachPrevention.union( (row - 1) * size + (col - 1), (r - 1) * size + (c - 1) );
                 }
             }
             if (row == 1){
                 uf.union( (row - 1) * size + (col - 1), size * size);
+                backWachPrevention.union( (row - 1) * size + (col - 1), size * size);
             }
             if (row == size){
                  uf.union( (row - 1) * size + (col - 1), (size * size) + 1);
@@ -68,8 +71,11 @@ public class Percolation {
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col){
-        int siteId = (row - 1) * size + (col - 1);
-        return ( uf.find(siteId) == uf.find(25) );
+        if (row < 1 || row > size || col < 1 || col > size) {
+            throw new IllegalArgumentException("row and col must be between 1 and n");
+        }
+        boolean backWashCondition = backWachPrevention.find((row - 1) * size + (col - 1)) == backWachPrevention.find(size*size);
+        return ( uf.find((row - 1) * size + (col - 1)) == uf.find(size*size)  && backWashCondition);
     }
 
     // returns the number of open sites
@@ -79,6 +85,6 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates(){
-        return uf.find(25) == uf.find(26);
+        return uf.find(size*size) == uf.find((size*size) + 1);
     }
 }
--- Module 2/PercolationStats.java
+++ Module 2/PercolationStats.java
@@ -1,41 +1,58 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
-
+    private double[] fractions;
+    private double CONFIDENCE_95 = 1.96;
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials){
         if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException("n and trials must be greater than 0");
         }
-        
-    }
+        fractions = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+            int openedSites = 0;
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniformInt(n) + 1;
+                int col = StdRandom.uniformInt(n) + 1;  
+                if (!percolation.isOpen(row, col)) {
+                    percolation.open(row, col);
+                    openedSites++;
+                }
+            }
+            fractions[i] = openedSites * 1.0 / (n * n);
+        }
+    }   
 
     // sample mean of percolation threshold
     public double mean(){
-        
+        return StdStats.mean(fractions);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev(){
-
+        return StdStats.stddev(fractions);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo(){
-
+        return mean() - (CONFIDENCE_95 * stddev() / Math.sqrt(fractions.length));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi(){
-
+        return mean() + (CONFIDENCE_95 * stddev() / Math.sqrt(fractions.length));
     }
 
    // test client (see below)
    public static void main(String[] args){
-    
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, trials);
+        StdOut.println("mean                    = " + ps.mean());
+        StdOut.println("stddev                  = " + ps.stddev());
+        StdOut.println("95% confidence interval = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
    }
-
 }
